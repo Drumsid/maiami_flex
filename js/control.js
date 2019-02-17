a259794e71516503ef50325ba18d562a0d97554c
Denis <den-sidnay@yandex.ru>
2019-02-17 21:29:57 +0300

    slide show

--- js/control.js
+++ js/control.js
@@ -13,6 +13,7 @@ $(function(){
 		);
 	})
 
+	//фиксированое меню
 	let offset = $('#nav-btn').offset();
 
 	$(window).scroll(function(){
@@ -25,20 +26,45 @@ $(function(){
 
 	});
 
-	if (window.innerWidth > 1024) {
-		$('#slide-s').slick({
-			slidesToShow: 3,
-	  		slidesToScroll: 1,
-	  		autoplay: true,
-	  		autoplaySpeed: 2000,
-		});
-	} else {
-		$('#slide-s').slick({
-			slidesToShow: 1,
-	  		slidesToScroll: 1,
-	  		autoplay: true,
-	  		autoplaySpeed: 2000,
-		});
-	}
+	// if (window.innerWidth > 1024) {
+	// 	$('#slide-s').slick({
+	// 		slidesToShow: 3,
+	//   		slidesToScroll: 1,
+	//   		autoplay: true,
+	//   		autoplaySpeed: 2000,
+	// 	});
+	// } else {
+	// 	$('#slide-s').slick({
+	// 		slidesToShow: 1,
+	//   		slidesToScroll: 1,
+	//   		autoplay: true,
+	//   		autoplaySpeed: 2000,
+	// 	});
+	// }
 
+    // слайдер слик
+		$('#slide-s').slick({
+		  slidesToShow: 3,
+		  slidesToScroll: 1,
+		  autoplay: true,
+		  autoplaySpeed: 2000,
+		  responsive: [ {
+		    breakpoint: 1024,
+		    settings: {
+		      slidesToShow: 1,
+		    },
+		  } ],
+		});
+
+
+		//модальное окно
+		$('.modal-box, .black-fill').hide();
+
+		$('.top-call').click(function() {
+			$('.modal-box, .black-fill').show();
+		});
+
+		$('.close-modal, .black-fill').click(function() {
+			$('.modal-box, .black-fill').hide();
+		});
 });
